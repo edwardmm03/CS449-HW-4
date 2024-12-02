20a08a37f20cb8038c612bd20e0aa671212ce69b
odomaj <odomaj@clarkson.edu>
2024-12-01 20:25:00 -0500

    added venv

--- src/tutorial.py
+++ src/tutorial.py
@@ -7,23 +7,27 @@ import numpy as np
 ##################################################################################################
 # policies
 
+
 def naive_policy(obs):
-	angle = obs[0]
-	return 0 if angle.any() < 0 else 1
+    angle = obs[0]
+    return 0 if angle.any() < 0 else 1
+
 
 def random_policy(obs):
-	angle = obs[1]
-	return 0 if np.random.uniform() < 0.5 else 1
+    angle = obs[1]
+    return 0 if np.random.uniform() < 0.5 else 1
+
 
 ##################################################################################################
 
-def naive_main( policy ):
-    debug = True;
+
+def naive_main(policy):
+    debug = True
     env = gym.make("CartPole-v0")
     obs = env.reset()
     env.render()
 
-    #episodic reinforcement learning
+    # episodic reinforcement learning
     totals = []
     for episode in range(100):
         episode_rewards = 0
@@ -35,16 +39,16 @@ def naive_main( policy ):
             time.sleep(0.1)
             episode_rewards += reward
             if terminated or trucated:
-                print ("Game over. Number of steps = ", step)
+                print("Game over. Number of steps = ", step)
                 env.render()
                 time.sleep(3.14)
         totals.append(episode_rewards)
-    print(np.mean(totals), np.std(totals), np.min(totals),np.max(totals))
-	
+    print(np.mean(totals), np.std(totals), np.min(totals), np.max(totals))
+
 
 ##################################################################################################
 
 if __name__ == "__main__":
-	naive_main( naive_policy )
+    naive_main(naive_policy)
 
 ##################################################################################################
