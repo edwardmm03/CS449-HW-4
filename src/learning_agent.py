1110127c3274220e09c7e61066445a2834b41d49
odomaj <143726318+odomaj@users.noreply.github.com>
2024-12-05 13:45:58 -0500

    saves agent

--- src/learning_agent.py
+++ src/learning_agent.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 from tf_keras.optimizers import Adam
 from tf_agents.networks.q_network import QNetwork
 from tf_agents.environments.tf_py_environment import TFPyEnvironment
@@ -10,16 +9,21 @@ from tf_agents.trajectories import from_transition
 from tf_agents.utils import common
 from typing import Union
 from tf_agents.environments import suite_gym
+from pathlib import Path
+import tensorflow as tf
 import numpy as np
 
-TRAINING_GAMES = 1
+TRAINING_GAMES = 10
 
 
 class LearningAgent:
     env: Union[TFPyEnvironment, None] = None
+    eval_env: Union[TFPyEnvironment, None] = None
+    global_step: Union[tf.Tensor, None] = None
     agent: Union[DqnAgent, None] = None
+    replay_buffer: Union[TFUniformReplayBuffer, None] = None
 
-    def __init__(self) -> None:
+    def __init__(self, file: Path) -> None:
         self.env = TFPyEnvironment(suite_gym.load("CartPole-v0"))
         self.eval_env = TFPyEnvironment(suite_gym.load("CartPole-v0"))
 
@@ -28,31 +32,41 @@ class LearningAgent:
             action_spec=self.env.action_spec(),
             fc_layer_params=(128, 128),
         )
+        self.global_step = tf.compat.v1.train.get_or_create_global_step()
         self.agent = DqnAgent(
             self.env.time_step_spec(),
             self.env.action_spec(),
             q_network=net,
             optimizer=Adam(learning_rate=1e-3),
             td_errors_loss_fn=common.element_wise_squared_loss,
-            train_step_counter=tf.Variable(0),
+            train_step_counter=self.global_step,
         )
         self.agent.initialize()
-
-    def train(self, epoch: int) -> None:
-        replay_buffer: TFUniformReplayBuffer = TFUniformReplayBuffer(
+        self.replay_buffer: TFUniformReplayBuffer = TFUniformReplayBuffer(
             data_spec=self.agent.collect_data_spec,
             batch_size=self.env.batch_size,
             max_length=100000,
         )
+        self.train_checkpointer = common.Checkpointer(
+            ckpt_dir=file,
+            max_to_keep=1,
+            agent=self.agent,
+            policy=self.agent.policy,
+            replay_buffer=self.replay_buffer,
+            global_step=self.global_step,
+        )
+        self.train_checkpointer.initialize_or_restore()
+
+    def train(self, epoch: int) -> None:
         for _ in range(epoch):
             time_step = self.env.reset()
             while not time_step.is_last():
                 action_step = self.agent.collect_policy.action(time_step)
                 next_time_step = self.env.step(action_step.action)
                 traj = from_transition(time_step, action_step, next_time_step)
-                replay_buffer.add_batch(traj)
+                self.replay_buffer.add_batch(traj)
                 time_step = next_time_step
-            experience = replay_buffer.as_dataset(
+            experience = self.replay_buffer.as_dataset(
                 sample_batch_size=64,
                 num_steps=2,
                 single_deterministic_pass=False,
@@ -69,8 +83,14 @@ class LearningAgent:
             if render:
                 self.eval_env.render(mode="human")
 
+    def save(self) -> None:
+        self.train_checkpointer.save(self.global_step)
+
 
 if __name__ == "__main__":
-    agent = LearningAgent()
+    agent = LearningAgent(
+        Path(__file__).parent.parent.joinpath("saves").joinpath("agent")
+    )
     agent.train(TRAINING_GAMES)
+    agent.save()
     agent.run_game()
